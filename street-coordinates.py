2534f3af8deedd8e63bdb86a09149f5fe7e2b6fe
Timo Laine <timo@timoroso.com>
2016-05-15 16:24:14 +0300

    reads street names from a csv file, uses them to call http://nominatim.openstreetmap.org, saves street coordinates as csv like this:
    streetname,lat,lon
    Pakila,24.9399631,60.243545
    Vartiokylä,25.0814407023908,60.2119555
    Brakvik,NA,NA
    Jätkäsaari,24.9116552,60.1570639
    Kulosaari,25.0035488212736,60.1855134

---
+++ street-coordinates.py
@@ -0,0 +1,64 @@
+import requests
+import requests_cache
+import urllib
+
+import time
+import csv
+
+import json
+
+import sys
+import os
+
+if not os.path.exists("data"):
+	os.makedirs("data")
+
+streets = []
+
+with open('data/streetnames.csv') as csvfile:
+	reader = csv.DictReader(csvfile)
+   	for row in reader:
+		streets.append(row['nimi'])
+
+
+requests_cache.install_cache('data/nominatim_cache', backend='sqlite')
+
+coordinates = []
+
+for i in streets:
+
+	query = "http://nominatim.openstreetmap.org/search.php?q={0}+helsinki&format=json"
+
+	response_dict = []
+
+	print("getting coordinates for " + i)
+	url = query.format(urllib.quote(i))
+	response = requests.get(url)
+	response_dict.append(response.json())
+	if not response.from_cache:
+		time.sleep(1)
+
+	if response_dict[0]:
+		lon = response_dict[0][0]['lon']
+		lat = response_dict[0][0]['lat']
+	else:
+		lon = "NA"
+		lat = "NA"
+
+	location = {'streetname' : i, 'lon' : lon, 'lat': lat}
+
+	coordinates.append(location)
+
+row = ['streetname','lat','lon']
+with open('data/street-coordinates.csv', 'wb') as f:
+	writer = csv.writer(f)
+	writer.writerow(row)
+for street in coordinates:
+	row = []
+	row.append(street['streetname'])
+	row.append(street['lon'])
+	row.append(street['lat'])
+	with open('data/street-coordinates.csv', 'ab') as f:
+		writer = csv.writer(f)
+		writer.writerow(row)
+
