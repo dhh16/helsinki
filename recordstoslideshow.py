f55f71f62eb999a384428b98adeae9e9a98478ca
Kari Laine <kklaine@gmail.com>
2016-05-19 09:54:42 +0300

    Additional info added to images

--- recordstoslideshow.py
+++ recordstoslideshow.py
@@ -8,6 +8,13 @@ import os
 import urllib
 import time
 
+import math
+import re
+
+import subprocess
+from subprocess import call
+
+
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -29,33 +36,64 @@ for street in data:
 
 years.sort()
 
-'''print years'''
-
 photos = []
-
-
-'''go through years
-	go through records
-		if year == record year:
-			append'''
 
 
 if not os.path.exists("images"):
 	os.makedirs("images")
+if not os.path.exists("images/original"):
+	os.makedirs("images/original")
+if not os.path.exists("images/processed"):
+	os.makedirs("images/processed")
 
+n=0
 
 for year in years:
         	for street in data:
                 	for record in street['records']:
                         	if 'year' in record.keys() and record['year'] == year:
-					'''create directory'''
                                 	for image in record['images']:
-               					'''photos.append('https://api.finna.fi' + image)'''
 						'''if file doesnt exist yet:'''
-						if not os.path.isfile("images/" + record['id'] + ".jpg"):
+						if not os.path.isfile("images/original/" + record['id'] + ".jpg"):
 							time.sleep(1)
-							urllib.urlretrieve ("https://api.finna.fi/Cover/Show?id=" + urllib.quote(record['id']) + "&fullres=1&index=0", "images/" + record['id'] + ".jpg")
+							urllib.urlretrieve ("https://api.finna.fi/Cover/Show?id=" + urllib.quote(record['id']) + "&fullres=1&index=0", "images/original/" + record['id'] + ".jpg")
 							'''print("https://api.finna.fi/Cover/Show?id=" + urllib.quote(record['id']) + "&fullres=1&index=0")'''
+						'''if the copy of the file with embedded text doesn't exist yet'''
+						if not os.path.isfile("images/processed" + record['id'] + ".jpg"):
+
+							if 'nonPresenterAuthors' in record:
+								for authorinfo in record['nonPresenterAuthors']:
+									author = authorinfo['name']
+									if(author == "Tuntematon"):
+										author = "unknown"
+							else:
+								author = "unknown"
+							subjects = ""
+							if 'subjects' in record:
+								for subject in record['subjects']:
+									subjects = subjects + "\n" + subject[0]
+							subprocess.call("convert images/original/" + record['id'] + ".jpg -fill white  -undercolor '#00000080'  -gravity Southeast -annotate +5+5 'Year: " + year + "\nAuthor: " + author + "' images/processed/"  + record['id'] + ".jpg", shell=True)
+							subprocess.call("convert images/processed/" + record['id'] + ".jpg -fill white  -undercolor '#00000080'  -gravity West -annotate +5+0 '" + subjects + "' images/processed/"  + record['id'] + ".jpg", shell=True)
+
+
+							value = record['value']
+							valuesize = 30 + (10 * abs(value))
+							if valuesize == 0:
+								valuesize = 30
+							if value < 0:
+								valuecolor = "'#FF4C4C'"
+							elif value == 0:
+								valuecolor = "white"
+							elif value > 0:
+								valuecolor = "'#00FF00'"
+							value = str(value)
+							subprocess.call("convert images/processed/" + record['id'] + ".jpg -fill " + valuecolor + "  -undercolor '#00000080' -pointsize " + str(valuesize) + " -gravity North -annotate +5+0 '" + value + "' images/processed/"  + record['id'] + ".jpg", shell=True)
+
+		n = n+1
+		completed = (100 * n / len(years))
+		print (str(completed) + "% completed")
+	
+						
 
 
 
